6e358c6c31e9931e25f085c8b11a2a5e2bfe0cc5
Eugene Pavlovskiy <infopavlovskiy@gmail.com>
2022-01-19 22:30:34 +0200

    2 COMMIT

--- main.js
+++ main.js
@@ -39,3 +39,44 @@ jQuery(document).ready(function () {
     );
 });
 
+
+const shareButton = document.querySelector('.pulse-button');
+const overlay = document.querySelector('.socialicons');
+
+const url = window.document.location.href;
+
+shareButton.addEventListener('click', () => {
+    if (navigator.share) {
+        navigator.share({
+            title: `${title}`,
+            url: `${url}`
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        }).catch(console.error);
+    } else {
+        overlay.classList.add('show-share');
+        shareButton.classList.add('show-share');
+    }
+})
+
+// Share = {
+//     facebook: function(purl, ptitle, pimg, text) {
+// 		url  = 'http://www.facebook.com/sharer.php?s=100';
+// 		url += '&p[title]='     + encodeURIComponent(ptitle);
+// 		url += '&p[summary]='   + encodeURIComponent(text);
+// 		url += '&p[url]='       + encodeURIComponent(purl);
+// 		url += '&p[images][0]=' + encodeURIComponent(pimg);
+// 		Share.popup(url);
+//     },
+//     linkedin: function(purl, ptitle, pimg, text) {
+// 		url  = 'https://www.linkedin.com/cws/share?url=[LINK]';
+// 		url += '&p[title]='     + encodeURIComponent(ptitle);
+// 		url += '&p[summary]='   + encodeURIComponent(text);
+// 		url += '&p[url]='       + encodeURIComponent(purl);
+// 		url += '&p[images][0]=' + encodeURIComponent(pimg);
+// 		Share.popup(url);
+//     },
+//     popup: function(url) {
+// 		window.open(url,'','toolbar=0,status=0,width=626,height=436');
+// 	}
+// }
